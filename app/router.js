970dbe5068c6cf55cb498e07dced094756c71b28
ma974235 <s10714140@gmail.com>
2022-02-23 16:48:35 +0800

    st

--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.board.read);
+  router.get('/index', controller.board.read);
   router.post('/insert',controller.board.create);
   router.get('/delete/:id',controller.board.destroy);
   router.post('/update',controller.board.update);
