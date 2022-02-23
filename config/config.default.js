90ba4c3cbc2e3fa5b148a95da2582a51697eadd8
ma974235 <s10714140@gmail.com>
2022-02-23 16:33:47 +0800

    all

---
+++ config/config.default.js
@@ -0,0 +1,64 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1645422226593_2394';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+  exports.view = {
+    defaultViewEngine:'nunjucks',
+    mapping: {
+      '.ejs': 'ejs',
+    },
+  };
+  exports.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: 'ma974232',
+      // 数据库名
+      database: 'board',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true
+    },  };
+  
+  config.cors = {
+    origin:'*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  };
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
